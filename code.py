8991b40dd01651f3f09efb30ef99d43cb615d369
Rodolphe <rodolpheandrieux@gmail.com>
2025-06-27 10:24:59 +0200

    Ajout de documentation pour améliorer la note pylint

--- code.py
+++ code.py
@@ -1,8 +1,15 @@
+"""Module mathématique simple."""
+
+
 class SimpleMath:
+    """Classe contenant des opérations mathématiques."""
+
     @staticmethod
     def addition(a, b):
+        """Retourne la somme de a et b."""
         return a + b
 
     @staticmethod
     def soustraction(a, b):
+        """Retourne la différence entre a et b."""
         return a - b
--- test.py
+++ test.py
@@ -1,12 +1,18 @@
+"""Tests unitaires pour SimpleMath."""
+
 import unittest
 from code import SimpleMath
 
 
 class TestSimpleMath(unittest.TestCase):
+    """Classe de tests pour les méthodes de SimpleMath."""
+
     def test_addition(self):
+        """Teste la méthode addition."""
         self.assertEqual(SimpleMath.addition(2, 3), 5)
 
     def test_soustraction(self):
+        """Teste la méthode soustraction."""
         self.assertEqual(SimpleMath.soustraction(5, 2), 3)
 
 
